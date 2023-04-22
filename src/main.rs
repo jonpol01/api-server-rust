ec0a75ca89e8b3fba4f70b587303babc0ab22e77
John Paul Soliva <soliva.johnpaul@icloud.com>
2023-04-22 20:44:40 +0900

    add/main: line comments

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,46 @@ use std::net::TcpStream;
 use std::io::prelude::*;
 // use thread
 use std::thread;
-// use time
-//use std::time::Duration;
-
-
 
 fn main() {
     // create a listener
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
+    // start accepting connections
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
 
+        // spawn a new thread to handle the connection
         thread::spawn(|| {
             handle_connection(stream);
         });
     }
 }
 
+// read and handle the incoming request
 fn handle_connection(mut stream: TcpStream) {
+    // buffer to read the request into
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
+    // check if the request is a GET request for the root directory
     let get = b"GET / HTTP/1.1\r\n";
-
-    // validate then open index.html
     let (status_line, filename) = if buffer.starts_with(get) {
+        // if it is, set the status line to 200 OK and the filename to index.html
         ("HTTP/1.1 200 OK\r\n", "index.html")
     } else {
+        // otherwise, set the status line to 404 NOT FOUND and the filename to 404.html
         ("HTTP/1.1 404 NOT FOUND\r\n", "404.html")
     };
     
-    // read file
+    // read the contents of the file into a string
     let contents = std::fs::read_to_string(filename).unwrap();
 
-    // create valid http/1.1 response
+    // create the response by combining the status line, content type, and contents of the file
     let response = format!(
         "{}Content-Length: {}\r\n\r\n{}",
         status_line,
@@ -52,7 +53,7 @@ fn handle_connection(mut stream: TcpStream) {
         contents
     );
 
-    // write response
+    // write the response to the stream and flush it to ensure that it gets sent immediately
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
  
